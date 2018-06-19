ac52057277cd72aa3542edbd1c4e8429e9a5b608
PRadipto Roy <proy.techworld@gmail.com>
2018-06-19 17:41:47 +0530

    Abstract

---
+++ javabasics/src/main/java/com/prsoft/javabucket/AbstractionPrac/Mytopabstractclass.java
@@ -0,0 +1,53 @@
+package com.prsoft.javabucket.AbstractionPrac;
+
+
+
+abstract class myTopabstract 
+{
+	public void display()
+	{
+		System.out.println("I am from abstract class");
+	}
+	
+	
+//	public abstract void dosometing();// This classes must be implemented why those class who extends abstract class
+//	public abstract void dosometingmore();//
+	
+}
+
+
+
+abstract class mysubclass extends myTopabstract  {
+	
+	
+	
+	public void display()
+	{
+		System.out.println("I am from subclass");
+	}
+	
+	
+	
+
+}
+
+public class Mytopabstractclass extends mysubclass
+{
+	
+	public void display()
+	{
+		System.out.println("I am from child of subclass");
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		
+		mysubclass obj=new Mytopabstractclass();
+		
+		obj.display();
+		
+		
+	}
+	
+}
